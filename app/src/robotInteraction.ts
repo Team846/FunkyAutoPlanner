6ee3492b51ff95ab7586fa2de886a10b67db3039
kgabc <kushgoel08@gmail.com>
2024-12-13 22:02:59 -0800

    Init. repo

---
+++ app/src/robotInteraction.ts
@@ -0,0 +1,11 @@
+
+
+//scp's single file from the public folder to the deploy folder in the 10.8.46.2 device
+export function scpFile(file : string){
+
+}
+
+//scp's entire contents of deploy folder in the public folder to the deploy folder in the 10.8.46.2 device
+export function scpFolder(){
+
+}
