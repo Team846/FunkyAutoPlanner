e761db1a8d1708e7097efc17416a7a41f1a4c764
shridharns <laks@Shridhars-MacBook-Air.local>
2024-12-26 14:14:12 -0800

    Finished the 2 functions in util.ts to convert field coordinates.

--- main/src/util.ts
+++ main/src/util.ts
@@ -1,15 +1,16 @@
+// Dimensions of the field: 26' 7" width and 54' 1" length. 
 export function fieldToPercX(x:number) {
-    return x;
+    return (x/649)*100;
 }
 
 export function percToFieldX(x:number) {
-    return x;
+    return (100*x)/649;
 }
 
 export function fieldToPercY(y:number) {
-    return y;
+    return (y/319)*100;
 }
 
 export function percToFieldY(y:number) {
-    return y;
+    return (100*y)/319;
 }
