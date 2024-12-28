06d35a25ff615f2638aaa96ef4ecd55535b575c7
shridharns <laks@Shridhars-MacBook-Air.local>
2024-12-27 20:09:32 -0800

    Updated functions that handle updated convertion.

--- main/src/util.ts
+++ main/src/util.ts
@@ -1,16 +1,16 @@
 // Dimensions of the field: 26' 7" width and 54' 1" length. 
-export function fieldToPercX(x:number) {
-    return (x/649)*100;
+export function fieldToPercX(x: number): number {
+    return (x / 649) * 100;
 }
 
-export function percToFieldX(x:number) {
-    return (100*x)/649;
+export function percToFieldX(x: number): number {
+    return (x * 649) / 100;
 }
 
-export function fieldToPercY(y:number) {
-    return (y/319)*100;
+export function fieldToPercY(y: number): number {
+    return (y / 319) * 100;
 }
 
-export function percToFieldY(y:number) {
-    return (100*y)/319;
+export function percToFieldY(y: number): number {
+    return (y * 319) / 100;
 }
