4b35025c44ccc194ce10b8b6075127604b83aa41
LakshanShridhar <luckylaksh0106@gmail.com>
2025-01-07 21:39:47 -0800

    Fixed the overlap in the action bubble.

--- main/src/AutoPlanner/ActionList/Action/Action.tsx
+++ main/src/AutoPlanner/ActionList/Action/Action.tsx
@@ -1,10 +1,17 @@
 import { Dispatch, MouseEventHandler, SetStateAction } from "react";
 import "./Action.css"
 
+var actions = ""; // Initialize actions as an empty variable initially.
+
 function Action({action, auto, setAuto} : {action:string, auto:((string | number)[] | number[][])[], setAuto: Dispatch<SetStateAction<((string | number)[] | number[][])[]>> }) {
 
     const handleClick =() =>{
         if (auto.length>0){
+            if (actions.length != 0) { // Condition to check that an action has taken place.
+                actions += ", " + action; // Concatenate the current action to the existing string of actions if this is true.
+            } else {
+                actions = action; // If not true, set the actions variable to the current action.
+            }
             var k=-1
             while (auto.length+k==-1 || typeof auto[auto.length+k][0]== 'string'){
                 k--;
@@ -13,7 +20,7 @@ function Action({action, auto, setAuto} : {action:string, auto:((string | number
             var y :number = ((auto.at(k) as number[][]).at(-1) as number[])[1];
             setAuto((prevAuto) => [
                 ...prevAuto, 
-                [action, x, y]
+                [actions, x, y]
             ]);
         }
     }
