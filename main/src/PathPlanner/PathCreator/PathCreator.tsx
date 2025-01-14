8841056544a7c74073a5f7ee0adb18c64d8e7bdf
ehang-1 <yihang0215@gmail.com>
2025-01-13 20:25:57 -0800

    Added "Additional Input" textbox so that you can leave any comments/remarks for any path

--- main/src/PathPlanner/PathCreator/PathCreator.tsx
+++ main/src/PathPlanner/PathCreator/PathCreator.tsx
@@ -1,21 +1,20 @@
-import { useState, SetStateAction, Dispatch} from "react";
+import { useState, SetStateAction, Dispatch } from "react";
 import "./PathCreator.css"
 import PageChooser from "../../PageChooser/PageChooser";
 
-function PathCreator({save, create, path, setPath, name, setName} : {save : Function, create: Function, path: boolean, setPath: Dispatch<SetStateAction<boolean>>, name:string, setName:Dispatch<SetStateAction<string>>}) {
-
+function PathCreator({save, create, path, setPath, name, setName, additionalText, setAdditionalText} : {save : Function, create: Function, path: boolean, setPath: Dispatch<SetStateAction<boolean>>, name:string, setName:Dispatch<SetStateAction<string>>, additionalText:string, setAdditionalText: Dispatch<SetStateAction<string>>}) {
 
     return (
       <div className="path-creator">
         <div className="header">
-          <div className="create-path" onClick={()=>{create(); setName("");}}> Create Path</div>
+          <div className="create-path" onClick={()=>{create(); setName(""); setAdditionalText("");}}> Create Path</div>
           <PageChooser onPath={path} setOnPath={setPath} />
           <div className="save-path" onClick={()=>{save();}}>Save Path</div>
         </div>
-        <input  className="path-name" placeholder="Path name here" value={name} onChange={evt => setName(evt.target.value)}/>
+        <input className="path-name" placeholder="Path name here" value={name} onChange={evt => setName(evt.target.value)}/>
+        <input className="additional-text" placeholder="Additional input here" value={additionalText} onChange={evt => setAdditionalText(evt.target.value)} />
       </div>
     );
-  }
+}
   
-  export default PathCreator;
-  
+export default PathCreator;
--- main/src/PathPlanner/PathPlanner.js
+++ main/src/PathPlanner/PathPlanner.js
@@ -1,90 +1,86 @@
 import { Dispatch, SetStateAction, useState } from "react";
-import Field, { PointToBeMade } from "./PathField/Field";
+import Field from "./PathField/Field";
 import PointAdder from "./PointAdder/PointAdder";
 import PointEditor from "./PointEditor/PointEditor";
 import PathCreator from "./PathCreator/PathCreator";
 import { scpFile } from "../robotInteraction";
 import PageChooser from "../PageChooser/PageChooser";
 import PathList from "./PathList/PathList";
-import "./PathPlanner.css"
-import { fieldToPercX, fieldToPercY, percToFieldX, percToFieldY} from "../util";
+import "./PathPlanner.css";
+import { fieldToPercX, fieldToPercY, percToFieldX, percToFieldY } from "../util";
 
-
-function PathPlanner({path, setPath}) {
-
+function PathPlanner({ path, setPath }) {
   const [curPath, setCurPath] = useState([]);
   const [pointOfInterest, setPointOfInterest] = useState();
   const [name, setName] = useState("");
+  const [additionalText, setAdditionalText] = useState("");
 
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [v, setV] = useState(0);
   const [r, setR] = useState(0);
 
-  const create = () =>{
+  const create = () => {
     setName("");
+    setAdditionalText("");  
   };
 
   const savePath = () => {
-    if (name ==""){
+    if (name === "") {
       console.error("Path must have a name!");
       return;
     }
-    if (curPath.length<1){
+    if (curPath.length < 1) {
       console.error("Path must have at least a point!");
       return;
     }
 
-    var msg="";
+    let msg = "";
 
-    for (var i=1; i<curPath.length; i++){
-      msg+=`P,${percToFieldX(curPath.at(i).CordX)},${percToFieldY(curPath.at(i).CordY)},${curPath.at(i).bearing},${curPath.at(i).velocity}\n`
+    for (let i = 1; i < curPath.length; i++) {
+      msg += `P,${percToFieldX(curPath[i].CordX)},${percToFieldY(curPath[i].CordY)},${curPath[i].bearing},${curPath[i].velocity}\n`;
     }
-    if (curPath.length==1){
-      //this is a point, not a path, save in points.lst
-      var addendMsg = `N,${name},${percToFieldX(curPath.at(0).CordX)},${percToFieldY(curPath.at(0).CordY)},${curPath.at(0).bearing},${curPath.at(0).velocity}`
-      window.api.send("readFromFile", `/deploy/points.lst`, "");
+    
+    if (curPath.length === 1) {
+      // This is a point, not a path; save in points.lst
+      const addendMsg = `N,${name},${percToFieldX(curPath[0].CordX)},${percToFieldY(curPath[0].CordY)},${curPath[0].bearing},${curPath[0].velocity}`;
+      window.api.send("readFromFile", "/deploy/points.lst", "");
       window.api.receive("fileData", (data) => {
-          if (data!=""){
-            window.api.send("writeToFile", `/deploy/points.lst`, data+"\n"+addendMsg);
-          }
-          else{
-            window.api.send("writeToFile", "/deploy/points.lst", addendMsg);
-          }
-          window.api.send("scpFile", `/deploy/points.lst`, `/points.lst`);
+        if (data !== "") {
+          window.api.send("writeToFile", "/deploy/points.lst", data + "\n" + addendMsg);
+        } else {
+          window.api.send("writeToFile", "/deploy/points.lst", addendMsg);
+        }
+        window.api.send("scpFile", "/deploy/points.lst", "/points.lst");
       });
-    }
-    else{
-      window.api.send("writeToFile", `/deploy/paths/${name}`, msg.substring(0, msg.length-1));
+    } else {
+      msg = `P,${percToFieldX(curPath[0].CordX)},${percToFieldY(curPath[0].CordY)},${curPath[0].bearing},${curPath[0].velocity},${additionalText}\n` + msg;
+      window.api.send("writeToFile", `/deploy/paths/${name}`, msg.substring(0, msg.length - 1));
       window.api.send("scpFile", `/deploy/paths/${name}`, `/paths/${name}`);
     }
-    msg=`P,${percToFieldX(curPath.at(0).CordX)},${percToFieldY(curPath.at(0).CordY)},${curPath.at(0).bearing},${curPath.at(0).velocity}\n`+msg
-
-
-    window.api.send("writeToFile", `/visualizer/paths/${name}`,  msg.substring(0, msg.length-1));
-
-
+    
+    window.api.send("writeToFile", `/visualizer/paths/${name}`, msg.substring(0, msg.length - 1));
   };
 
-  const onPointClick =(i) =>{
+  const onPointClick = (i) => {
     setPointOfInterest(i);
     setX(percToFieldX(curPath[i].CordX));
     setY(percToFieldY(curPath[i].CordY));
     setV(curPath[i].velocity);
-    setR(curPath[i].bearing)
+    setR(curPath[i].bearing);
   }
+
   return (
     <div className="PathPlanner">
       <header className="Path-header">
-        <Field onPointClick={onPointClick}listOfPoints={curPath} setListOfPoints={setCurPath}/>
+        <Field onPointClick={onPointClick} listOfPoints={curPath} setListOfPoints={setCurPath} />
         <div className="path-box">
-        <PointEditor pointOfInterest={pointOfInterest} path={curPath} setPath={setCurPath} x={x} setX={setX} y={y} setY={setY} v={v} setV={setV} r={r} setR={setR}/>
-          <div style={{ display: 'flex', flexDirection: "column"}}>
-            <PathCreator setPath={setPath} path={path} create={create} save={savePath} name={name} setName={setName}/>
+          <PointEditor pointOfInterest={pointOfInterest} path={curPath} setPath={setCurPath} x={x} setX={setX} y={y} setY={setY} v={v} setV={setV} r={r} setR={setR}/>
+          <div style={{ display: 'flex', flexDirection: "column" }}>
+            <PathCreator setPath={setPath} path={path} create={create} save={savePath} name={name} setName={setName} additionalText={additionalText} setAdditionalText={setAdditionalText}/>
             <PointAdder path={curPath} setPath={setCurPath}/>
           </div>
-
-        <PathList setPath={setCurPath} setName={setName}/>
+          <PathList setPath={setCurPath} setName={setName}/>
         </div>
       </header>
     </div>
