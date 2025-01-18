114a389f3908e55eba999aed2061176ac6ffb79f
Mihir Shankar <mihir.shankar20@gmail.com>
2025-01-17 17:30:20 -0800

    Can drag and drop actions in auto box

--- main/src/AutoPlanner/AutoFormer/AutoFormer.tsx
+++ main/src/AutoPlanner/AutoFormer/AutoFormer.tsx
@@ -3,7 +3,23 @@ import PageChooser from "../../PageChooser/PageChooser";
 import "./AutoFormer.css"
 
 function AutoFormer({onPath, setOnPath}:{onPath:boolean, setOnPath:Dispatch<SetStateAction<boolean>>}) {
-    const [autoList, setAutoList] = useState<string[]>([]);
+    const [autoList, setAutoList] = useState<string[]>(["go forward", "go back", "jump", 'climb', 'dance', 'hop']);
+
+    const handleDragStart = (e: React.DragEvent<HTMLDivElement>, index: number) => {
+      e.dataTransfer.setData('ItemIndex', index.toString())
+    }
+    const handleDrop = (e:React.DragEvent<HTMLDivElement>, index:number) => {
+      const ItemIndex = parseInt(e.dataTransfer.getData("ItemIndex"));
+      if (ItemIndex === index) return;
+      const newList = [...autoList];
+      const [deletedItem] = newList.splice(ItemIndex, 1);
+      newList.splice(index, 0, deletedItem);
+      setAutoList(newList);
+    }
+    const enableDropping = (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+    };
+    
     return (
       <div className="AutoFormer">
         <header className="Auto-former-header">
@@ -16,24 +32,43 @@ function AutoFormer({onPath, setOnPath}:{onPath:boolean, setOnPath:Dispatch<SetS
         
         <div className="creator-box">
           <p>Creator</p> 
-          <div className="auto-box">
-            {autoList.length > 0 ?(
-              autoList.map((item, index) => {
-                return (
-                  <div className="Action" key = {index}
-                  onContextMenu = {(e) => {
-                    e.preventDefault();
-                    setAutoList((prevList) => prevList.filter((e,i) => i !== index));
-                  }}
+          <div className="auto-box" onDragOver={enableDropping} > 
+          {autoList.length > 0 ? (
+            autoList.map((item, index) => {
+              return (
+                <div key={item}>
+                  <div
+                    className="drop-zone"
+                    onDrop={(e) => handleDrop(e, index)}
+                  >
+                  </div>
+                  <div
+                    className="Action"
+                    draggable
+                    onDragStart={(e) => handleDragStart(e, index)}
+                    onContextMenu={(e) => {
+                      e.preventDefault();
+                      setAutoList((prevList) =>
+                        prevList.filter((e, i) => i !== index)
+                      );
+                    }}
                   >
                     {item}
-                    
                   </div>
-                );
-              })
-            ):(
+                </div>
+              );
+            })
+          ) : (
             <span className="box-desc">Drag an auto to get started</span>
-          )} 
+          )}
+          {autoList.length === 0 && (
+            <div
+              className="drop-zone"
+              onDragOver={enableDropping}
+              onDrop={(e) => handleDrop(e, autoList.length)}
+            >
+            </div>
+          )}
           </div>
         </div>
       </div>
