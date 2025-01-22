6a04c44f40ac376b76ed0a79b1175497682c70b4
Mihir Shankar <mihir.shankar20@gmail.com>
2025-01-21 18:53:38 -0800

    Actions reorder in auto page when drag and dropped ,ui updated

--- main/src/AutoPlanner/AutoFormer/AutoFormer.tsx
+++ main/src/AutoPlanner/AutoFormer/AutoFormer.tsx
@@ -1,18 +1,36 @@
 import { Dispatch, SetStateAction, useState } from "react";
 import PageChooser from "../../PageChooser/PageChooser";
 import "./AutoFormer.css"
+import { PointToBeMade } from "../../PathPlanner/PathField/Field";
 
-function AutoFormer({onPath, setOnPath, createAuto, name, setName, saveAuto, namedAutoList, setNamedAutoList, autoList, setAutoList}:{onPath:boolean, setOnPath:Dispatch<SetStateAction<boolean>>, createAuto:Function, name:string, setName:Dispatch<SetStateAction<string>>, saveAuto:Function, namedAutoList:string[], setNamedAutoList:Dispatch<SetStateAction<string[]>>, autoList:string[], setAutoList:Dispatch<SetStateAction<string[]>>}) {
+function AutoFormer({onPath, setOnPath, createAuto, name, setName, saveAuto, namedAutoList, setNamedAutoList, autoList, setAutoList}:{onPath:boolean, setOnPath:Dispatch<SetStateAction<boolean>>, createAuto:Function, name:string, setName:Dispatch<SetStateAction<string>>, saveAuto:Function, namedAutoList:string[], setNamedAutoList:Dispatch<SetStateAction<string[]>>, autoList:(string|number[]|PointToBeMade[])[], setAutoList:Dispatch<SetStateAction<(string|number[]|PointToBeMade[])[]>>}) {
     const handleDragStart = (e: React.DragEvent<HTMLDivElement>, index: number) => {
       e.dataTransfer.setData('ItemIndex', index.toString())
     }
     const handleDrop = (e:React.DragEvent<HTMLDivElement>, index:number) => {
       const ItemIndex = parseInt(e.dataTransfer.getData("ItemIndex"));
       if (ItemIndex === index) return;
+
       const newNamedList = [...namedAutoList];
       const newList = [...autoList];
       const [deletedNamedItem] = newNamedList.splice(ItemIndex, 1);
-      const [deletedItem] = newList.splice(ItemIndex, 1);
+      const [deletedItem] = newList.splice(ItemIndex, 1)
+
+      let nearestPathCoords = null
+      for (let i = index-1; i>=0; i--){
+        const item = newList[i]
+        if (Array.isArray(item) && item[item.length-1] instanceof PointToBeMade){
+          nearestPathCoords = item[item.length-1]
+          break;
+        }
+      }
+      if (Array.isArray(deletedItem) && typeof deletedItem[0] === "string"){
+        const endCoord = nearestPathCoords;
+        if (endCoord instanceof PointToBeMade){
+        deletedItem[1] = endCoord.CordX
+        deletedItem[2] = endCoord.CordY;
+        }
+      }
       newNamedList.splice(index, 0, deletedNamedItem);
       newList.splice(index, 0, deletedItem);
       setNamedAutoList(newNamedList);
